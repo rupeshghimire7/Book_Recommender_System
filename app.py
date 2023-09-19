dc9120223f0af6cb577090159f676c419a0de086
Rupesh Ghimire <rupeshghimire007@gmail.com>
2023-09-19 13:08:51 +0545

    Flask deployment: Popularity Based Filtered Page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import pickle
+
+df = pickle.load(open('popular_books.pkl','rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html',book_name = list(df['Book-Title'].values),author=list(df['Book-Author'].values),image=list(df['Image-URL-L'].values),year=list(df['Year-Of-Publication'].values),rating_count=list(df['Rating_Count'].values),avg_rating=list(df['Avg_Ratings'].values)
+                           )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

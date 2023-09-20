83c4dcc0314feaa749f5867b23a4375e7ec8cda5
Rupesh Ghimire <rupeshghimire007@gmail.com>
2023-09-20 09:25:47 +0545

    Added template filter to make ratings floor max at 5

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def index():
     return render_template('index.html',book_name = list(df['Book-Title'].values),author=list(df['Book-Author'].values),image=list(df['Image-URL-L'].values),year=list(df['Year-Of-Publication'].values),rating_count=list(df['Rating_Count'].values),avg_rating=list(df['Avg_Ratings'].values)
                            )
 
+@app.template_filter('floor')
+def floor(x:float):
+    return int(x)
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
